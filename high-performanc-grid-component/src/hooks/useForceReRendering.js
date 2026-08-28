db1832186fa421712fcd84879b2cd7356b4bb17f
Simin Shoeibi <simin.shoaibi@proton.me>
2026-08-28 14:57:27 +0400

    -

--- high-performanc-grid-component/src/hooks/useForceReRendering.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const useForceReRendering = () => {
-    const [tmp, setTmp] = React.useState(0);
-    return () => setTmp(tmp => tmp + 1);
-}
-
-export default useForceReRendering;
--- json-generator/generator-packages/README.md
+++
@@ -1,3 +0,0 @@
-# Generator Packages
-
-These are packages for JSON Generator, released on famous marketplaces, made based on the original CLI tool.
